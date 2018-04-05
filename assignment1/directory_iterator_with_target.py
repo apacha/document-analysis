d0165ba15d8c1ee3a5fefd4e54e613b91cd767b7
Alexander Pacha <alexander.pacha@gmail.com>
2018-04-05 17:16:00 +0200

    Removing skip-functionality, because no frames are annotated as being skipped

--- assignment1/directory_iterator_with_target.py
+++ assignment1/directory_iterator_with_target.py
@@ -4,9 +4,8 @@ import numpy as np
 from keras import backend
 from keras.preprocessing import image
 from keras.preprocessing.image import DirectoryIterator, ImageDataGenerator
-from six.moves import range
 
-import train_model
+from annotation_loader import load_mapping
 
 
 class DirectoryIteratorWithTarget(DirectoryIterator):
@@ -31,7 +30,6 @@ class DirectoryIteratorWithTarget(DirectoryIterator):
         batch_y = np.zeros((batch_size,) + (8,), dtype=backend.floatx())
 
         # build batch of image data
-        skipped_files = 0
         for i, j in enumerate(index_array):
             fname = self.filenames[j]
             img = image.load_img(os.path.join(self.directory, fname),
@@ -40,19 +38,14 @@ class DirectoryIteratorWithTarget(DirectoryIterator):
             x = self.image_data_generator.random_transform(x)
             x = self.image_data_generator.standardize(x)
             if fname in self.filename_to_target_mapping:
-                batch_x[i - skipped_files] = x
-                batch_y[i - skipped_files] = np.asarray(self.filename_to_target_mapping[fname])
-            else:
-                skipped_files += 1
+                batch_x[i] = x
+                batch_y[i] = np.asarray(self.filename_to_target_mapping[fname])
 
-        if skipped_files > 0:
-            batch_x = np.delete(batch_x, np.s_[batch_size - skipped_files:batch_size], 0)
-            batch_y = np.delete(batch_y, np.s_[batch_size - skipped_files:batch_size], 0)
         return batch_x, batch_y
 
 
 if __name__ == "__main__":
-    filename_to_target_mapping = train_model.load_mapping()
+    filename_to_target_mapping = load_mapping()
     batch_size = 16
     image_data_generator = DirectoryIteratorWithTarget("data/images", ImageDataGenerator(), filename_to_target_mapping,
                                                        target_size=(192, 108), batch_size=batch_size)
