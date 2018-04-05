d66d1a8d3a68d1cf4d92881d025ed31ed22adf76
Alexander Pacha <alexander.pacha@gmail.com>
2018-04-05 13:41:11 +0200

    Putting all images into the same folder, called all_objects

--- assignment1/download_and_prepare_dataset.py
+++ assignment1/download_and_prepare_dataset.py
@@ -5,15 +5,14 @@ from tqdm import tqdm
 
 if __name__ == "__main__":
     dataset_directory = "data"
+    output_directory = os.path.join(dataset_directory, "images", "all_objects")
+    os.makedirs(output_directory, exist_ok=True)
 
     backgrounds = os.listdir(os.path.join(dataset_directory, "page-detection"))
 
     for background in tqdm(backgrounds, desc="Processing backgrounds"):
         avi_files = glob(os.path.join(dataset_directory, "page-detection", background, "*.avi"))
         annotation_files = glob(os.path.join(dataset_directory, "page-detection", background, "*.xml"))
-
-        output_directory = os.path.join(dataset_directory, "images", background)
-        os.makedirs(output_directory, exist_ok=True)
 
         for avi_file in tqdm(avi_files, desc="Processing avi file"):
             vidcap = cv2.VideoCapture(avi_file)
@@ -23,7 +22,7 @@ if __name__ == "__main__":
             success = True
 
             while success:
-                output_name = "{0}-frame{1}.jpg".format(file_name, one_based_index)
+                output_name = "{0}-{1}-frame{2}.jpg".format(background, file_name, one_based_index)
                 cv2.imwrite(os.path.join(output_directory, output_name), image)
                 success, image = vidcap.read()
                 one_based_index += 1
