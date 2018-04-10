1e9b03cb73e8cf7764b438cec6d1a4b8f834ad4f
Alexander Pacha <alexander.pacha@gmail.com>
2018-04-10 15:00:29 +0200

    Allowing to script train_model with various parameters by allowing to use command-line parameters

--- assignment1/train_model.py
+++ assignment1/train_model.py
@@ -1,3 +1,4 @@
+import argparse
 import datetime
 from glob import glob
 import os
@@ -12,31 +13,47 @@ from directory_iterator_with_target import DirectoryIteratorWithTarget, load_map
 from models.ConfigurationFactory import ConfigurationFactory
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_directory", type=str, default="data",
+                        help="The directory, that is used for storing the images during training")
+    parser.add_argument("--model_name", type=str, default="res_net_50",
+                        help="The model used for training the network. Run >python models/ConfigurationFactory.py to get a list of all available configurations")
+    parser.add_argument("--width", default=400, type=int, help="Width of the input-images for the network in pixel")
+    parser.add_argument("--height", default=224, type=int, help="Height of the input-images for the network in pixel")
+    parser.add_argument("--batch_size", default=16, type=int,
+                        help="The minibatch-size for training. Reduce to 8 or 4 if your graphics card runs out of "
+                             "memory, but keep high otherwise to speed up training")
+    flags, unparsed = parser.parse_known_args()
+    dataset_directory = flags.dataset_directory
+    model_name = flags.model_name
+    image_width = flags.width
+    image_height = flags.height
+    batch_size = flags.batch_size
+
     filename_to_target_mapping = load_mapping()
     start_of_training = datetime.date.today()
-    batch_size = 16
-    training_data_generator = DirectoryIteratorWithTarget("data/images-training",
+    training_data_generator = DirectoryIteratorWithTarget(os.path.join(dataset_directory, "images-training"),
                                                           ImageDataGenerator(),
                                                           filename_to_target_mapping,
-                                                          target_size=(224, 400),
+                                                          target_size=(image_height, image_width),
                                                           batch_size=batch_size)
-    validation_data_generator = DirectoryIteratorWithTarget("data/images-validation",
+    validation_data_generator = DirectoryIteratorWithTarget(os.path.join(dataset_directory, "images-validation"),
                                                             ImageDataGenerator(),
                                                             filename_to_target_mapping,
-                                                            target_size=(224, 400),
+                                                            target_size=(image_height, image_width),
                                                             batch_size=batch_size)
-    test_data_generator = DirectoryIteratorWithTarget("data/images-test",
-                                                            ImageDataGenerator(),
-                                                            filename_to_target_mapping,
-                                                            target_size=(224, 400),
-                                                            batch_size=batch_size)
+    test_data_generator = DirectoryIteratorWithTarget(os.path.join(dataset_directory, "images-test"),
+                                                      ImageDataGenerator(),
+                                                      filename_to_target_mapping,
+                                                      target_size=(image_height, image_width),
+                                                      batch_size=batch_size)
 
-    training_configuration = ConfigurationFactory.get_configuration_by_name("res_net_50", 400, 224)
+    training_configuration = ConfigurationFactory.get_configuration_by_name(model_name, image_width, image_height)
     training_steps_per_epoch = np.math.ceil(training_data_generator.samples / training_data_generator.batch_size)
     validation_steps_per_epoch = np.math.ceil(validation_data_generator.samples / validation_data_generator.batch_size)
     test_steps_per_epoch = np.math.ceil(validation_data_generator.samples / validation_data_generator.batch_size)
 
-    best_model_path = "trained_model.h5"
+    best_model_path = "{0}_{1}.h5".format(start_of_training, training_configuration.name())
     monitor_variable = 'val_mean_absolute_error'
     model_checkpoint = ModelCheckpoint(best_model_path, monitor=monitor_variable, save_best_only=True, verbose=1)
     early_stop = EarlyStopping(monitor=monitor_variable,
