529d7cba70a0a635055129f5df08bf5e63d66a95
Alexander Pacha <alexander.pacha@gmail.com>
2018-05-07 15:58:11 +0200

    Adding various run-configurations, that were explored

--- assignment1/train_model.ps1
+++ assignment1/train_model.ps1
@@ -7,16 +7,51 @@ cd $pathToSourceRoot
 echo "Appending required paths to temporary PYTHONPATH"
 $env:PYTHONPATH = "$($pathToGitRoot);$($pathToSourceRoot)"
 
+Start-Transcript -path "$($pathToTranscript)\2018-04-19_res_net_50_pyramid_800x448_relative_standardize.txt" -append
+python "$($pathToSourceRoot)\train_model.py" --dataset_directory data --model_name res_net_50_pyramid --width 800 --height 448 --batch_size 8 --use_relative_coordinates --standardize
+Stop-Transcript
 
-Start-Transcript -path "$($pathToTranscript)\2018-04-13_res_net_50_gap_1920x1080_relative-coordinates.txt" -append
-python "$($pathToSourceRoot)\train_model.py" --dataset_directory data --model_name res_net_50_gap --width 1920 --height 1080 --batch_size 1 --use_relative_coordinates
+exit
+
+Start-Transcript -path "$($pathToTranscript)\2018-04-17_inception_resnet_v2_gap_400x224_relative_standardize.txt" -append
+python "$($pathToSourceRoot)\train_model.py" --dataset_directory data --model_name inception_resnet_v2_gap --width 400 --height 224 --batch_size 16 --use_relative_coordinates --standardize
+Stop-Transcript
+
+Start-Transcript -path "$($pathToTranscript)\2018-04-16_res_net_50_gap_preserve_space_400x224_relative_standardize.txt" -append
+python "$($pathToSourceRoot)\train_model.py" --dataset_directory data --model_name res_net_50_gap_preserve_space --width 400 --height 224 --batch_size 16 --use_relative_coordinates --standardize
+Stop-Transcript
+
+Start-Transcript -path "$($pathToTranscript)\2018-04-16_res_net_50_gap_400x224_relative_standardize.txt" -append
+python "$($pathToSourceRoot)\train_model.py" --dataset_directory data --model_name res_net_50_gap --width 400 --height 224 --batch_size 16 --use_relative_coordinates --standardize
+Stop-Transcript
+
+Start-Transcript -path "$($pathToTranscript)\2018-04-15_res_net_50_gap_960x540_relative-coordinates.txt" -append
+python "$($pathToSourceRoot)\train_model.py" --dataset_directory data --model_name res_net_50_gap --width 960 --height 540 --batch_size 4 --use_relative_coordinates
+Stop-Transcript
+
+Start-Transcript -path "$($pathToTranscript)\2018-04-15_xception_gap_960x540_relative-coordinates.txt" -append
+python "$($pathToSourceRoot)\train_model.py" --dataset_directory data --model_name xception_gap --width 960 --height 540 --batch_size 4 --use_relative_coordinates
+Stop-Transcript
+
+Start-Transcript -path "$($pathToTranscript)\2018-04-15_inception_resnet_v2_gap_960x540_relative-coordinates.txt" -append
+python "$($pathToSourceRoot)\train_model.py" --dataset_directory data --model_name inception_resnet_v2_gap --width 960 --height 540 --batch_size 4 --use_relative_coordinates
 Stop-Transcript
 
 Start-Transcript -path "$($pathToTranscript)\2018-04-13_res_net_50_400x224_relative-coordinates.txt" -append
 python "$($pathToSourceRoot)\train_model.py" --dataset_directory data --model_name res_net_50 --width 400 --height 224 --batch_size 16 --use_relative_coordinates
 Stop-Transcript
 
-exit
+Start-Transcript -path "$($pathToTranscript)\2018-04-14_xception_gap_400x224_relative-coordinates.txt" -append
+python "$($pathToSourceRoot)\train_model.py" --dataset_directory data --model_name xception_gap --width 400 --height 224 --batch_size 8 --use_relative_coordinates
+Stop-Transcript
+
+Start-Transcript -path "$($pathToTranscript)\2018-04-14_inception_resnet_v2_gap_400x224_relative-coordinates.txt" -append
+python "$($pathToSourceRoot)\train_model.py" --dataset_directory data --model_name inception_resnet_v2_gap --width 400 --height 224 --batch_size 8 --use_relative_coordinates
+Stop-Transcript
+
+Start-Transcript -path "$($pathToTranscript)\2018-04-15_res_net_50_gap_1920x1080_relative-coordinates.txt" -append
+python "$($pathToSourceRoot)\train_model.py" --dataset_directory data --model_name res_net_50_gap --width 1920 --height 1080 --batch_size 1 --use_relative_coordinates
+Stop-Transcript
 
 Start-Transcript -path "$($pathToTranscript)\2018-04-13_res_net_50_gap_400x224_relative-coordinates.txt" -append
 python "$($pathToSourceRoot)\train_model.py" --dataset_directory data --model_name res_net_50_gap --width 400 --height 224 --batch_size 8 --use_relative_coordinates
