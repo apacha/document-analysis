6c92ff0727aeb731f689cfbaa4e2c61bec7bf4c6
Alexander Pacha <alexander.pacha@gmail.com>
2018-06-04 00:02:46 +0200

    Adding script to predict from a trained model. Unfortunately only decodes garbage right now.

---
+++ assignment2/predict.py
@@ -0,0 +1,69 @@
+import argparse
+import itertools
+import os
+from glob import glob
+
+import keras
+import numpy as np
+from PIL import Image
+from keras.backend import ctc_decode
+from keras.preprocessing import image
+from tqdm import tqdm
+
+import annotation_loader
+import dataset_loader
+from lines_to_window_converter import read_grayscale_image_and_add_padding
+from models.ConfigurationFactory import ConfigurationFactory
+
+
+def predict(dataset_directory: str, model_path: str, image_height: int, image_width: int):
+    pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_directory", type=str, default="data",
+                        help="The directory, that is used for storing the images during training")
+    parser.add_argument("--model_path", type=str, default="2018-06-03_simple_64x1900.h5",
+                        help="The trained model")
+    parser.add_argument("--height", default=64, type=int, help="Height of the input-images for the network in pixel")
+    parser.add_argument("--width", default=1900, type=int, help="Width of the input-images for the network in pixel")
+
+    flags, unparsed = parser.parse_known_args()
+
+    # predict(flags.dataset_directory, flags.model_path, flags.width, flags.height, flags.use_relative_coordinates,
+    #         flags.standardize)
+
+    dataset_directory = "data"
+    model_path = "2018-06-03_simple_1900x64_val_loss_3.2.h5"
+    model_path = "2018-06-03_simple_1900x64.h5"
+    image_height, image_width = 64, 1900
+    absolute_max_string_length = 146
+    alphabet_length = 77
+    images = glob(dataset_directory + "/lines-test/*.png")
+
+    mapping = annotation_loader.load_mapping()
+    alphabet = annotation_loader.load_alphabet_from_samples(mapping)
+    print("Loading model...")
+    best_configuration = ConfigurationFactory.get_configuration_by_name("simple", image_height, image_width,
+                                                                        alphabet_length, absolute_max_string_length)
+    best_model = best_configuration.model()
+    best_model.load_weights(model_path)
+
+    inputs, _ = dataset_loader.load_dataset(dataset_directory, "test", mapping, image_height, image_width, absolute_max_string_length)
+    prediction = best_model.predict(inputs)
+
+    softmax_activations = prediction[1]
+
+    # This tries to decode the resulting activations, but I have no idea, how to use it, so I decode it manually
+    for index, sample_activations in enumerate(softmax_activations):
+        out_best = list(np.argmax(sample_activations[2:], 1))
+        out_best = [k for k, g in itertools.groupby(out_best)]
+        decoded = ""
+        for c in out_best:
+            if c == len(alphabet):
+                continue # CTC Blank
+            else:
+                decoded += (alphabet[c])
+
+        ground_truth = inputs['ground_truth'][index]
+        print("Decoded: {0}\nGT: {1}".format(decoded, ground_truth))
