1614c080947b8bfcc8f2d96f0d96059d4b5f04f5
Alexander Pacha <alexander.pacha@gmail.com>
2018-06-09 22:18:23 +0200

    Revert "delete validation directory in dataset splitter", because the method should stay the same and potentially split the dataset into train/val/test. If only train/test is used, the respective call should be used instead of mutilating the existing method without properly renaming it.
    This reverts commit 3bac4ad65f04d7c377773fe1a4a32594d7a296f6.

--- assignment2/dataset_splitter.py
+++ assignment2/dataset_splitter.py
@@ -37,16 +37,19 @@ def _get_randomized_sample_pairs(i_am_printed_directory: str, seed_for_reproduci
 def split_dataset_into_training_and_test_sets(dataset_directory: str = "data"):
     return split_dataset_into_training_validation_and_test_sets(dataset_directory, 0.8, 0.2)
 
-
-def split_dataset_into_training_validation_and_test_sets(dataset_directory: str = "data", training_precentage=0.8,
+def split_dataset_into_training_validation_and_test_sets(dataset_directory: str = "data", training_precentage=0.6,
                                                          test_percentage=0.2):
     i_am_printed_directory = os.path.join(dataset_directory, "I AM printed")
     training_directory = os.path.join(dataset_directory, "training")
+    validation_directory = os.path.join(dataset_directory, "validation")
     test_directory = os.path.join(dataset_directory, "test")
 
     if os.path.exists(training_directory):
         shutil.rmtree(training_directory)
     os.makedirs(training_directory)
+    if os.path.exists(validation_directory):
+        shutil.rmtree(validation_directory)
+    os.makedirs(validation_directory)
     if os.path.exists(test_directory):
         shutil.rmtree(test_directory)
     os.makedirs(test_directory)
@@ -60,12 +63,13 @@ def split_dataset_into_training_validation_and_test_sets(dataset_directory: str 
 
     randomized_files = _get_randomized_sample_pairs(i_am_printed_directory)
 
-    # Corresponds to a 80%, 20% split for train and test
     test_pairs = randomized_files[0:math.ceil(len(randomized_files) * test_percentage)]
-    training_pairs = randomized_files[len(test_pairs):len(randomized_files)]
+    training_pairs = randomized_files[
+                     len(test_pairs):len(test_pairs) + math.ceil(len(randomized_files) * training_precentage)]
+    validation_pairs = randomized_files[len(training_pairs) + len(test_pairs):len(randomized_files)]
 
-    print("Split a total of {0} samples into two sets with {1} training and {2} test samples.".format(
-        len(randomized_files), len(training_pairs), len(test_pairs)
+    print("Split a total of {0} samples into three sets with {1} training, {2} validation and {3} test samples.".format(
+        len(randomized_files), len(training_pairs), len(validation_pairs), len(test_pairs)
     ))
 
     for i in range(0, len(test_pairs)):
@@ -74,6 +78,9 @@ def split_dataset_into_training_validation_and_test_sets(dataset_directory: str 
     for i in range(0, len(training_pairs)):
         _copy_image_and_annotation(i_am_printed_directory, training_directory, training_pairs[i])
 
+    for i in range(0, len(validation_pairs)):
+        _copy_image_and_annotation(i_am_printed_directory, validation_directory, validation_pairs[i])
+
 
 if __name__ == "__main__":
-    split_dataset_into_training_validation_and_test_sets("data")
+    split_dataset_into_training_and_test_sets("data")
